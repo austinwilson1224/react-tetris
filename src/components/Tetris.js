cdf20abdc7d58bd5003a8d665bb4aae597f6ac45
austinwilson1224 <austin1224@gmail.com>
2020-09-04 10:44:01 -0700

    rotation

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -21,7 +21,7 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer, rotatePlayer] = usePlayer();
     const [stage, setStage] = useStage(player);
 
 
@@ -71,6 +71,8 @@ const Tetris = () => {
                 movePlayer(1);
             } else if (keyCode === 40) {
                 dropPlayer();
+            } else if (keyCode === 38) {
+                rotatePlayer(stage, 1);
             }
         }
     }
